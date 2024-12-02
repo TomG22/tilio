ea1af5abad3bda692a4640bf896351682f545ed5
efattelluofa <efattelL@arizona.edu>
2024-12-02 16:15:43 -0700

    Added routes for leaderboards.

--- server.js
+++ server.js
@@ -10,15 +10,43 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mernstack', {
+mongoose.connect('mongodb://localhost:27017/tilio', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-// Basic Route
-app.get('/', (req, res) => {
+
+const boardSchema = new mongoose.Schema ({
+  name:  String,
+  score: Number,
+  time:  ISODate
+});
+
+const LeaderboardScore = mongoose.model('LeaderboardScore', boardSchema);
+const LeaderboardScore = mongoose.model('LeaderboardTime', boardSchema);
+
+// Route for client to recieve updated leaderboard Score information
+app.get('/leaderboardScore', async (req, res) => {
+  try {
+    // if score is the same, then we shoudl sort by time
+    const leaderboard = await LeaderboardScore.find().sort({ score:1 });
+    res.json({ success; true, tilio });
+  } catch (error) {
+    res.status(500);
+  }
+});
+
+// Route for client to recieve updated leaderboard by Time information
+app.get('/leaderboardTime', async (req, res) => {
+  try {
+    // if score is the same, then we shoudl sort by time
+    const leaderboard = await LeaderboardTime.find().sort({ score:1 });
+    res.json({ success; true, tilio });
+  } catch (error) {
+    res.status(500);
+  }
 });
 
 // Express Staiic route for client
