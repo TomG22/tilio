b632dbcdd7f24587624fff74f7b26709e27a2ec3
efattelluofa <efattelL@arizona.edu>
2024-12-10 09:57:26 -0700

    Added schemas and configured the route for static leaderboard.

--- server.js
+++ server.js
@@ -17,12 +17,42 @@ mongoose.connect('mongodb://localhost:27017/mernstack', {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
+const userSchema = new mongoose.Schema({
+  username: {type: String,  required: true},
+  allTimeHighScore: {type: Number, required: true}
+  
+});
+
+const scoreSchema = new mongoose.Schema({
+  score: {type: String,  required: true},
+  time: {type: ISODate, required: true}
+  
+});
+
+const gameSchema = new mongoose.Schema({
+  userID: User._id,
+  scoreID: Score._id,
+  score: String,
+  board: [Number]
+  startTime: ISODate,
+  endTime: ISODate,
+  winTime: ISODate
+})
+
+const User = mongooose.model('User', userSchema);
+
+const Score = mongoose.model('Score', scoreSchema);
+
+const GameStatic = mongoose.model('GameStaic', gameSchema);
+
+const GameLive = mongoose.model('GameLive', gameSchema);
+
 app.use(express.static('public_html'));
 
 app.use(express.json());
 
 app.get("/leaderboard+static", (req, res) => {
-  res.send();
+  res.send(await User.find().sort({ score: 1}));
 });
 
 
