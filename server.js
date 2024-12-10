9f33785d15569cd02eb6fcec0c82a58088c8a958
efattelluofa <efattelL@arizona.edu>
2024-12-10 12:08:40 -0700

    Fixed syntax errors.

--- server.js
+++ server.js
@@ -25,23 +25,24 @@ const userSchema = new mongoose.Schema({
 
 const scoreSchema = new mongoose.Schema({
   score: {type: String,  required: true},
-  time: {type: ISODate, required: true}
+  time: {type: Date, required: true}
   
 });
 
-const gameSchema = new mongoose.Schema({
-  userID: User._id,
-  scoreID: Score._id,
-  score: String,
-  board: [Number],
-  startTime: ISODate,
-  endTime: ISODate,
-  winTime: ISODate
-})
-
-const User = mongooose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 const Score = mongoose.model('Score', scoreSchema);
+
+const gameSchema = new mongoose.Schema({
+  userID: mongoose.Schema.Types.ObjectId,
+  scoreID: mongoose.Schema.Types.ObjectId,
+  score: String,
+  board: [Number],
+  startTime: Date,
+  endTime: Date,
+  winTime: Date
+})
+
 
 const GameStatic = mongoose.model('GameStaic', gameSchema);
 
@@ -51,12 +52,22 @@ app.use(express.static('public_html'));
 
 app.use(express.json());
 
-app.get("/leaderboard/static", (req, res) => {
-  res.send(await GameStatic.find().sort({ score: 1}));
+app.get("/leaderboard/static", async (req, res) => {
+  try {
+    const leaderboard = await GameStatic.find().sort({ score: 1});
+    res.send(leaderboard);
+  } catch (error){
+
+  }
 });
 
-app.get("/leaderboard/live", (req, res) => {
-  res.send(await GameLive.find().sort({ score: 1}));
+app.get("/leaderboard/live", async (req, res) => {
+  try {
+    const leaderboard = await GameLive.find().sort({ score: 1});
+    res.send(leaderboard);
+  } catch (error){
+
+  }
 });
 
 app.post("/leaderboard/live/update", async (req, res) => {
@@ -71,7 +82,7 @@ app.post("/leaderboard/live/update", async (req, res) => {
     const result = await GameLive.findOneAndUpdate(
       { username },
       { score, updatedAt: Date.now() },
-      { new: true, upstart true }
+      { new: true, upstart: true }
     );
 
     if (winTime != 0) {
@@ -81,7 +92,7 @@ app.post("/leaderboard/live/update", async (req, res) => {
         { endTime, updatedAt: Date.now() },
         { winTime, updatedAt: Date.now() },
         { board, updatedAt: Date.now() },
-        { new: true, upstart true }
+        { new: true, upstart: true }
       );
     }
 
